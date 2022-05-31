2efe6bc1b8b387049669d021a096515dc874db8c
zjp369 <605146581@qq.com>
2022-05-31 20:35:03 +0800

    不再创建临时文件(.sql)，使用管道进行进程间通信，提高程序执行效率。

--- shp2pg.bat
+++ shp2pg.bat
@@ -8,7 +8,8 @@ dropdb -U postgres %db_name%
 createdb -U postgres %db_name%
 psql -U postgres -d %db_name% -c "CREATE EXTENSION IF NOT EXISTS postgis"
 
-shp2pgsql -D world.shp > world.shp.sql
-psql -U postgres %db_name% < world.shp.sql
+rem shp2pgsql -D world.shp > world.shp.sql
+rem psql -U postgres %db_name% < world.shp.sql
+shp2pgsql -D world.shp | psql -U postgres %db_name%
 
 pause
