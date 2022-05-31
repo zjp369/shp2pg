b63de4fd0c8676fe5ac83af1a37ee475e567393a
zjp369 <605146581@qq.com>
2022-05-31 20:54:57 +0800

    使用iconv进行编码转换，修正不能导入问题

--- shp2pg.bat
+++ shp2pg.bat
@@ -15,7 +15,9 @@ rem shp2pgsql -D world.shp | psql -U postgres %db_name%
 set keyword=*.shp
 for /f  %%i in ('dir /b/s %keyword%') do (
 	echo %%~ni
-	shp2pgsql -D -W gbk %%i | psql -U postgres %db_name%
+	rem shp2pgsql -D -W gbk %%i | psql -U postgres %db_name%
+	shp2pgsql -D -W gbk %%i > %%i.sql
+	iconv -f gbk -t utf-8 %%i.sql | psql -U postgres %db_name%
 )
 
 pause
