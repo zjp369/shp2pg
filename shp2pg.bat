54a23d99cb8e792c9c0a5bda6eae0cf3c7ddabaa
zjp369 <605146581@qq.com>
2022-05-31 20:32:58 +0800

    使用-D选项，提高执行效率。“-D Use postgresql dump format (defaults to SQL insert statements).”

--- shp2pg.bat
+++ shp2pg.bat
@@ -8,7 +8,7 @@ dropdb -U postgres %db_name%
 createdb -U postgres %db_name%
 psql -U postgres -d %db_name% -c "CREATE EXTENSION IF NOT EXISTS postgis"
 
-shp2pgsql world.shp > world.shp.sql
+shp2pgsql -D world.shp > world.shp.sql
 psql -U postgres %db_name% < world.shp.sql
 
 pause
