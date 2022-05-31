488a929ec9227dcbf4615a51e35c96696fed7d61
zjp369 <605146581@qq.com>
2022-05-31 21:08:30 +0800

    在Windows操作系统中，必须将中文编码（gb18030、gbk、gb2312）文件名称，转换为utf-8编码的数据库表名称

--- shp2pg.bat
+++ shp2pg.bat
@@ -13,10 +13,16 @@ rem psql -U postgres %db_name% < world.shp.sql
 rem shp2pgsql -D world.shp | psql -U postgres %db_name%
 
 set keyword=*.shp
-for /f  %%i in ('dir /b/s %keyword%') do (
+for /f %%i in ('dir /b/s %keyword%') do (
 	echo %%~ni
 	rem shp2pgsql -D -W gbk %%i | psql -U postgres %db_name%
-	shp2pgsql -D -W gbk %%i | iconv -f gbk -t utf-8 | psql -U postgres %db_name%
+	rem shp2pgsql -D -W gbk %%i | iconv -f gbk -t utf-8 | psql -U postgres %db_name%
+	for /f %%j in ('echo %%~ni ^| iconv -f gb18030 -t utf-8') do (
+		rem echo %%j
+		shp2pgsql -D -W gb18030 %%i %%j > %%i.sql
+	)
+	psql -U postgres %db_name% < %%i.sql
+	rem del %%i.sql
 )
 
 pause
