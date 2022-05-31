beb90836d7440abda8b81302aa79026ee694e002
zjp369 <605146581@qq.com>
2022-05-31 20:30:31 +0800

    增加shp2pg.bat以及相关数据

---
+++ shp2pg.bat
@@ -0,0 +1,14 @@
+﻿@echo off
+
+set db_name=world
+set path=%path%;D:\PostgreSQL\bin;D:\Green\Git\bin;D:\Green\Git\usr\bin
+set PGPASSWORD=123456
+
+dropdb -U postgres %db_name%
+createdb -U postgres %db_name%
+psql -U postgres -d %db_name% -c "CREATE EXTENSION IF NOT EXISTS postgis"
+
+shp2pgsql world.shp > world.shp.sql
+psql -U postgres %db_name% < world.shp.sql
+
+pause
